03bcecd7496fe53f390365c7bdd307d114e66522
itacdonev <itacdonev@gmail.com>
2024-02-06 14:16:26 +0100

    typo

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     description="Customized machine learning package for practical projects.",
     name="finmetrika-ml",
     version="0.1.0",
-    packages=find_packages(include=["finmetrika-ml", 
+    packages=find_packages(include=["finmetrika_ml", 
                                     "finmetrika.*"], # include all subpmodules
                            ),
     python_requires=">=3.9",
-    install_requires=[
-        "pytorch>=2.0"
-    ]
+    #install_requires=[
+    #    "pytorch>=2.0.1"
+    #]
 )
