0f26d481215d91cd4301628bf66dfbafc84a5c16
itacdonev <itacdonev@gmail.com>
2024-02-06 11:39:21 +0100

    Changed install_requires downgraded pytorch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     author="Ita Cirovic Donev",
-    description="Customized machine learning packgae for practical projects.",
+    description="Customized machine learning package for practical projects.",
     name="finmetrika-ml",
     version="0.1.0",
     packages=find_packages(include=["finmetrika-ml", 
@@ -10,6 +10,6 @@ setup(
                            ),
     python_requires=">=3.9",
     install_requires=[
-        "pytorch>=2.2.0"
+        "pytorch>=2.0"
     ]
 )
